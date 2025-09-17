0c4620b1d1fe360d7baaaee9462360b3f4dc8201
chhenao <carloshenao2004@hotmail.com>
2025-09-17 21:59:35 +0200

    Add files via upload

--- plantilla-peluqueria/script.js
+++ plantilla-peluqueria/script.js
@@ -1,20 +1,32 @@
- 
-  // Mostrar el modal al cargar la página
-    window.onload = function() {
-      const accepted = localStorage.getItem("cookiesAccepted");
-      if (!accepted) {
-        document.getElementById("cookieModal").style.display = "flex";
-      }
-    };
 
-    function acceptCookies() {
-      localStorage.setItem("cookiesAccepted", "true");
-      document.getElementById("cookieModal").style.display = "none";
-    }
+// Mostrar el modal al cargar la página
+window.onload = function () {
+  const accepted = localStorage.getItem("cookiesAccepted");
+  if (!accepted) {
+    document.getElementById("cookieModal").style.display = "flex";
+  }
+};
 
-    function declineCookies() {
-      localStorage.setItem("cookiesAccepted", "false");
-      document.getElementById("cookieModal").style.display = "none";
-    }
-  
- 
+function acceptCookies() {
+  localStorage.setItem("cookiesAccepted", "true");
+  document.getElementById("cookieModal").style.display = "none";
+}
+
+function declineCookies() {
+  localStorage.setItem("cookiesAccepted", "false");
+  document.getElementById("cookieModal").style.display = "none";
+}
+
+//menu navegacion
+const menuToggle = document.querySelector('.menu-toggle');
+const menuClose = document.querySelector('.menu-close');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.add('active');
+});
+
+menuClose.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+});
+
