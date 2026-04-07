cf87b497f535828b024bf7811e102ac854a03a9e
chhenao <carloshenao2004@hotmail.com>
2026-04-07 22:04:32 +0200

    Delete plantilla-bogados directory

--- plantilla-bogados/footer.js
+++
@@ -1,6 +0,0 @@
-// Actualiza el año automáticamente
-document.addEventListener("DOMContentLoaded", () => {
-  const footerBottom = document.querySelector(".footer-bottom");
-  const year = new Date().getFullYear();
-  footerBottom.innerHTML = `&copy; ${year} Bufete Legal & Asociados. Todos los derechos reservados.`;
-});
