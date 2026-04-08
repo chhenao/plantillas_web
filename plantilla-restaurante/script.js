ee135bbbaed3cc8e8ebc126a1e9f663a0df464ac
chhenao <carloshenao2004@hotmail.com>
2026-04-08 22:28:39 +0200

    Add files via upload

--- plantilla-restaurante/script.js
+++ plantilla-restaurante/script.js
@@ -1,38 +1,35 @@
- 
-  // Mostrar el modal al cargar la página
-    window.onload = function() {
-      const accepted = localStorage.getItem("cookiesAccepted");
-      if (!accepted) {
-        document.getElementById("cookieModal").style.display = "flex";
-      }
-    };
+// Cookies
+window.onload = function() {
+  const accepted = localStorage.getItem("cookiesAccepted");
+  if (!accepted) {
+    document.getElementById("cookieModal").style.display = "flex";
+  }
+};
 
-    function acceptCookies() {
-      localStorage.setItem("cookiesAccepted", "true");
-      document.getElementById("cookieModal").style.display = "none";
-    }
+function acceptCookies() {
+  localStorage.setItem("cookiesAccepted", "true");
+  document.getElementById("cookieModal").style.display = "none";
+}
 
-    function declineCookies() {
-      localStorage.setItem("cookiesAccepted", "false");
-      document.getElementById("cookieModal").style.display = "none";
-    }
+function declineCookies() {
+  localStorage.setItem("cookiesAccepted", "false");
+  document.getElementById("cookieModal").style.display = "none";
+}
 
-    /* menu navegacion */
-    const hamburger = document.getElementById("hamburger");
+// Menú hamburguesa
+const hamburger = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  hamburger.classList.toggle("active");
-});
-
-// Cerrar menú al hacer clic en un enlace
-document.querySelectorAll(".nav-links a").forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.classList.remove("active");
-    hamburger.classList.remove("active");
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
   });
-});
 
-  
- 
+  document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+      hamburger.classList.remove("active");
+    });
+  });
+}
